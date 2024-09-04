d3ef00ea2a15f249843e238831bd3262d305707a
ISCAR-CSUOHIO <157401897+ISCAR-CSUOHIO@users.noreply.github.com>
2024-09-03 20:01:20 -0400

    Add files via upload

---
+++ Dipen_Bhuva/A_novel_continuous_authentication_method_using_biometrics_for_IOT_devices/CA-code/auth.sh
@@ -0,0 +1,19 @@
+#SBATCH --job-name auth
+#SBATCH --account=PFS0238
+#SBATCH -J ondemand/sys/myjobs/basic_sequential
+#SBATCH --time=00:30:00
+#SBATCH --nodes=2
+#SBATCH --exclusive
+
+# The following is an example of a single-processor sequential job that uses $TMPDIR as its working area.
+# This batch script copies the script file and input file from the directory the
+# qsub command was called from into $TMPDIR, runs the code in $TMPDIR,
+# and copies the output file back to the original directory.
+#   https://www.osc.edu/supercomputing/batch-processing-at-osc/job-scripts
+#
+# Move to the directory where the job was submitted
+#
+module load miniconda3
+source ~/.bashrc
+conda activate ECG
+python Authentication.py 
